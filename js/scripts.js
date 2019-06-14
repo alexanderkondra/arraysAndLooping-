85717ab69af99bb84bdb49f9d0924a069ba6b609
Alexander Kondra <alexanderkondra@icloud.com>
2019-06-14 10:44:34 -0700

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
+//User interface logic
+
 $(document).ready(function() {
 
+//Select form and submit button
+  $("form#input-form").submit(function(event) {
+    event.preventDefault();
 
+//Create variable what user inputs
+    var userInput = $("input#number").val();
 
-
-
-
+//Grab information and create variable so i can create function for it. Little confused on this one.
+    userInput = checkForAllExceptions(userInput);
+    $(.output).text(userInput);
+    $("#result").show();
+  });
 
 });
